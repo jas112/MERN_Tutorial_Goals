9a13975d3aadd83ce19487e2e0d579cc4ea6fcf1
jas112 <jas.sandmaster@gmail.com>
2022-06-18 00:45:12 -0700

    added veiws/, dashboard, register, login...add components folder...added frontend routes with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './views/Dashboard';
+import Register from './views/Register';
+import Login from './views/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Mern-Tutorial</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
